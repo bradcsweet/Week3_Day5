8f0c2d19c4d443ff72a1e515d13586bca2df060b
bradcsweet <bradcsweet@gmail.com>
2017-10-13 10:01:08 -0400

    Initial commit

---
+++ Week3_Day5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Cat> catArmy = new List<Cat>();
+
+            List<Turtle> turtleArmy = new List<Turtle>();
+
+            Random rand = new Random();
+
+
+
+        }
+    }
+}
---
+++ Week3_Day5/Turtle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Day5
+{
+    class Turtle
+    {
+        static int turtleCount;
+
+        public Turtle()
+        {
+            turtleCount++;
+        }
+
+
+
+
+
+    }
+}
